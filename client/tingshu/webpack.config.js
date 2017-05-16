336eb15277e087fc7858160b71e56bd86210e9fd
fws407296762 <fws407296762@qq.com>
2017-05-17 00:06:05 +0800

    重新搭建项目，添加一些随机文件，下载后要删掉

--- client/tingshu/webpack.config.js
+++ client/tingshu/webpack.config.js
@@ -7,38 +7,75 @@ const node_modules = path.join(__dirname, "/node_modules/");
 
 const projectName = path.basename(__dirname); //获取项目名称
 
-module.exports = {
-    entry: {
-        router: path.join(src, "static/router.js"),
-        vue: path.join(node_modules, "vue/dist/vue.min.js"),
-        "vue-router": path.join(node_modules, "vue-router/dist/vue-router.min.js")
+module.exports = [
+    {
+        entry: {
+            vue: path.join(node_modules, "vue/dist/vue.min.js"),
+            "vue-router": path.join(node_modules, "vue-router/dist/vue-router.min.js")
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "static/" + projectName + "/js/lib/[name].js?v=[hash]",
+            libraryTarget: "amd"
+        },
+        plugins: [
+            new CopyWebpackPlugin([
+                {
+                    from: "./src/static/sea.js",
+                    to: path.resolve(__dirname, "dist/static/" + projectName + "/js/lib")
+                }
+            ]),
+            new HtmlWebpackPlugin({
+                filename: "/views/" + projectName + "/index.html",
+                template: path.join(src, "views/index.html"),
+                chunks: []
+            })
+        ],
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "static/"+projectName+"/js/[name].js?v=[hash]",
-        libraryTarget: "amd"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist/static/js"),
-        compress: true,
-        port: 9000
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-                from:"./src/static/require.js",
-                to:path.resolve(__dirname, "dist/static/"+projectName+"/js")
-            }
-        ]),
-        new HtmlWebpackPlugin({
-            filename:"views/"+projectName+"/index.html",
-            template:path.join(src, "views/index.html"),
-            chunks:[]
-        })
-    ],
-    module: {
-        rules: [
-
-        ]
+    {
+        entry: {
+            router: path.join(src, "static/router.js")
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "static/" + projectName + "/js/[name].js?v=[hash]",
+            libraryTarget: "amd"
+        }
     }
-}
+]
+
+// module.exports = {
+//     entry: {
+//         router: path.join(src, "static/router.js"),
+//         vue: path.join(node_modules, "vue/dist/vue.min.js"),
+//         "vue-router": path.join(node_modules, "vue-router/dist/vue-router.min.js")
+//     },
+//     output: {
+//         path: path.resolve(__dirname, "dist"),
+//         filename: "static/"+projectName+"/js/[name].js?v=[hash]",
+//         libraryTarget: "amd"
+//     },
+//     devServer: {
+//         contentBase: path.join(__dirname, "dist/static/js"),
+//         compress: true,
+//         port: 9000
+//     },
+//     plugins: [
+//         new CopyWebpackPlugin([
+//             {
+//                 from:"./src/static/require.js",
+//                 to:path.resolve(__dirname, "dist/static/"+projectName+"/js")
+//             }
+//         ]),
+//         new HtmlWebpackPlugin({
+//             filename:"views/"+projectName+"/index.html",
+//             template:path.join(src, "views/index.html"),
+//             chunks:[]
+//         })
+//     ],
+//     module: {
+//         rules: [
+
+//         ]
+//     }
+// }
